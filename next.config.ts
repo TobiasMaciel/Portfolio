a062a314b63d1fbf61c638dd941cb1bf4c39b781
TobiasMaciel <tobiasmaciel03@gmail.com>
2026-04-01 17:54:47 -0300

    fix: Basepath

--- next.config.ts
+++ next.config.ts
@@ -5,9 +5,8 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  // Si tu repositorio se llama "Portfolio" y tu URL será "usuario.github.io/Portfolio", descomentá esta línea y la de abacjo:
-  // basePath: "/Portfolio",
-  // assetPrefix: "/Portfolio/",
+  basePath: process.env.GITHUB_ACTIONS ? "/Portfolio" : "",
+  assetPrefix: process.env.GITHUB_ACTIONS ? "/Portfolio/" : "",
   allowedDevOrigins: ['192.168.1.5', 'localhost']
 };
 
